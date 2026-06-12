bebc0e36cc35d4acdea18ceeffcb443437c21400
chubrik333228337-cell <chubrik333228337@gmail.com>
2026-06-13 01:25:18 +0300

    Update TelegramSender.java

--- app/src/main/java/com/smstofred/TelegramSender.java
+++ app/src/main/java/com/smstofred/TelegramSender.java
@@ -7,6 +7,10 @@ import java.net.URLEncoder;
 public class TelegramSender {
     private static final String BOT_TOKEN = "8262219232:AAFR1sA9XrGTua8unBdA0FG20bbAjBlF0q8"; // замени на свой, баля
 
+    public static String getBotToken() {
+        return BOT_TOKEN;
+    }
+
     public static void sendToChat(long chatId, String text) {
         new Thread(() -> {
             try {
