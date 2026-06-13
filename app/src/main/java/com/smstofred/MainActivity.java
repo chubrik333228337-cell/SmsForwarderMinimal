700dc8693f0f3c65b64cb6ac1fc29d28f2f1fa56
chubrik333228337-cell <chubrik333228337@gmail.com>
2026-06-13 03:32:14 +0300

    Update MainActivity.java

--- app/src/main/java/com/smstofred/MainActivity.java
+++ app/src/main/java/com/smstofred/MainActivity.java
@@ -6,9 +6,6 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.Toast;
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 public class MainActivity extends android.app.Activity {
     private static final int PERMISSION_REQUEST_CODE = 100;
@@ -16,7 +13,7 @@ public class MainActivity extends android.app.Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Запрашиваем разрешения
+
         String[] perms;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             perms = new String[]{
@@ -32,15 +29,17 @@ public class MainActivity extends android.app.Activity {
                     Manifest.permission.ANSWER_PHONE_CALLS
             };
         }
+
         boolean allGranted = true;
         for (String p : perms) {
-            if (ContextCompat.checkSelfPermission(this, p) != PackageManager.PERMISSION_GRANTED) {
+            if (checkSelfPermission(p) != PackageManager.PERMISSION_GRANTED) {
                 allGranted = false;
                 break;
             }
         }
+
         if (!allGranted) {
-            ActivityCompat.requestPermissions(this, perms, PERMISSION_REQUEST_CODE);
+            requestPermissions(perms, PERMISSION_REQUEST_CODE);
         } else {
             startService();
             finish();
@@ -48,7 +47,7 @@ public class MainActivity extends android.app.Activity {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (requestCode == PERMISSION_REQUEST_CODE) {
             boolean ok = true;
             for (int r : grantResults) {
